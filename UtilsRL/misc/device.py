9e2a567e32b10d12e453cba4609d9a3ecffeaec6
typoverflow <typoverflow@outlook.com>
2022-03-20 18:29:36 +0800

    fix: certain versions of cuda may not display reserved memory

--- UtilsRL/misc/device.py
+++ UtilsRL/misc/device.py
@@ -1,4 +1,5 @@
 import os
+from time import get_clock_info
 import torch
 
 
@@ -33,22 +34,34 @@ def select_device(id=None):
         return ret
 
 def select_free_cuda():
-    import numpy as np
-    cmd1 = os.popen("nvidia-smi -q -d Memory | grep -A4 GPU | grep Total")
-    total = cmd1.readlines()
-    cmd1.close()
-    
-    cmd2 = os.popen("nvidia-smi -q -d Memory | grep -A4 GPU | grep Reserved")
-    reserved = cmd2.readlines()
-    cmd2.close()
-    
-    cmd3 = os.popen("nvidia-smi -q -d Memory | grep -A4 GPU | grep Used")
-    used = cmd3.readlines()
-    cmd3.close()
-    
     def get_volume(t):
         return np.asarray([int(i.split()[2]) for i in t])
-    
-    total, used, reserved = get_volume(total), get_volume(used), get_volume(reserved)
-    return np.argmax(total-used-reserved)
+
+    try: 
+        import numpy as np
+        cmd1 = os.popen("nvidia-smi -q -d Memory | grep -A4 GPU | grep Total")
+        total = cmd1.readlines()
+        cmd1.close()
+        
+        cmd2 = os.popen("nvidia-smi -q -d Memory | grep -A4 GPU | grep Reserved")
+        reserved = cmd2.readlines()
+        cmd2.close()
+        
+        cmd3 = os.popen("nvidia-smi -q -d Memory | grep -A4 GPU | grep Used")
+        used = cmd3.readlines()
+        cmd3.close()
+        
+        def get_volume(t):
+            return np.asarray([int(i.split()[2]) for i in t])
+        
+        total, used, reserved = get_volume(total), get_volume(used), get_volume(reserved)
+        return np.argmax(total-used-reserved)
+    except Exception as e:
+        cmd4 = os.popen("nvidia-smi -q -d Memory | grep -A4 GPU | grep Free")
+        free = cmd4.readlines()
+        cmd4.close()
+        
+        free = get_volume(free)
+        return np.argmax(free)
+        
     
