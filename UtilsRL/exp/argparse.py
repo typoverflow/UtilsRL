6d263377b0da6832b4e24e1275abd33893a3d57f
typoverflow <typoverflow@outlook.com>
2022-03-23 19:47:26 +0800

    update: argument parsing and convert logic

--- UtilsRL/exp/argparse.py
+++ UtilsRL/exp/argparse.py
@@ -2,6 +2,8 @@ import argparse
 from collections import OrderedDict
 from types import ModuleType, FrameType
 from typing import Optional, OrderedDict, Union, Dict, Any
+
+from numpy import isin, nested_iters
     
 from UtilsRL.misc.namespace import NameSpace, NameSpaceMeta
 from UtilsRL.misc.chore import safe_eval
@@ -14,52 +16,90 @@ def register_argparse_callback(key, func):
     Register a callback function which is specific to UtilsRL use. 
     """
     argparse_callbacks[key] = func
-
-def parse_args(args: Optional[Union[str, dict, ModuleType]] = None, convert=True) -> Union[NameSpace, Dict[str, Any]]:
+    
+def get_key(_key):
+    if _key.startswith("--"):
+        return _key[2:]
+    elif _key.startswith("-"):
+        return _key[1:]
+    else:
+        return _key
+    
+def parse_cmd_args(convert=True):
+    cmd_parser = argparse.ArgumentParser()
+    _, cmd_args = cmd_parser.parse_known_args()
+    num = len(cmd_args)//2
+    cmd_args = dict(zip([get_key(cmd_args[2*i]) for i in range(num)], [cmd_args[2*i+1] for i in range(num)]))
+    args = NameSpace("cmd_args", {}, nested=True) if convert else {}
+    for key, value in cmd_args.items():
+        keys = key.split(".")
+        this = args
+        for subkey in keys[:-1]:
+            if convert:
+                this[subkey] = this.get(subkey, NameSpace(subkey, {}, nested=True))
+            else:
+                this[subkey] = this.get(subkey, {})
+            this = this[subkey]
+        this[keys[-1]] = safe_eval(value)
+    return args
+    
+def parse_args(path: Optional[Union[str, dict, ModuleType]] = None, convert=True) -> Union[NameSpace, Dict[str, Any]]:
     """
     Parse args from json file, yaml, python file or plain old dict. 
     Command-line argumnets will be parsed as well, and will be used to overwrite
         the init arguments. 
     
-    Args: 
+    Args:
         args: can be string or python module object. If it is string, it will be 
             interpreted as the path to the config file. If it is a python module, 
             then its attributes will be extracted to from an argument dict. 
         convert: whether to convert the final argument dict to a Namespace class, 
             which brings lots of convenience. Default to True. 
     """
-    if isinstance(args, str):
-        if args.endswith(".json"):
+    cmd_args = parse_cmd_args(convert=convert)
+    
+    # parse args from config files or modules
+    if isinstance(path, str):
+        if path.endswith(".json"):
             import json
-            with open(args, "r") as fp:
-                args = json.load(fp)
-        elif args.endswith(".yaml") or args.endswith(".yml"):
+            with open(path, "r") as fp:
+                file_args = json.load(fp)
+        elif path.endswith(".yaml") or path.endswith(".yml"):
             import yaml
-            with open(args, "r") as fp:
-                args = yaml.safe_load(fp)
-        elif args.endswith(".py"):
+            with open(path, "r") as fp:
+                file_args = yaml.safe_load(fp)
+        elif path.endswith(".py"):
             import importlib.util
-            spec = importlib.util.spec_from_file_location("config_module", args)
+            spec = importlib.util.spec_from_file_location("config_module", path)
             foo = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(foo)
-            args = _parse_args_from_module(foo)
-    elif isinstance(args, ModuleType):
-        args = _parse_args_from_module(args)
-    elif args is None:
-        args = {}
-        
-    # update with command line arguments
-    cmd_parser = argparse.ArgumentParser()
-    _, unknown = cmd_parser.parse_known_args()
-    args = update_args(args, unknown)
+            file_args = _parse_args_from_module(foo)
+    elif isinstance(path, ModuleType):
+        file_args = _parse_args_from_module(path)
+    elif path is None:
+        file_args = {}
     
-    # convert or not
-    if not isinstance(args, dict):
-        raise TypeError("Unsupported args type: {}".format(type(args)))
+    file_args = NameSpace("args", file_args, nested=True) if convert else file_args
+        
+    # update with cmd args
+    def traverse_add(old, new):
+        for new_key, new_value in new.items():
+            if new_key not in old:
+                old[new_key] = new_value
+            elif type(old[new_key]) != type(new_value):
+                old[new_key] = new_value
+            else:
+                if (convert and isinstance(new_value, NameSpaceMeta)) \
+                    or (not convert and isinstance(new_value, dict)):
+                    traverse_add(old[new_key], new_value)
+                else:
+                    old[new_key] = new_value
+                
+    traverse_add(file_args, cmd_args)
     
     # check if there is a callback
     for key in argparse_callbacks:
-        _args = args
+        _args = file_args
         _keys = key.split(".")
         for k in _keys:
             _args = _args.get(k, None)
@@ -67,12 +107,9 @@ def parse_args(args: Optional[Union[str, dict, ModuleType]] = None, convert=True
                 break
         else:
             ret = argparse_callbacks[key](_args)
-            args = update_args(args, ret)
+            file_args = update_args(file_args, ret)
     
-    if convert: 
-        return NameSpace("args", args, nested=convert)
-    else:
-        return args
+    return file_args
     
 def update_args(args, new_args: Optional[Union[dict, list]] = None):
     """Update the arguments with a flat dict or list of key-value pairs.
