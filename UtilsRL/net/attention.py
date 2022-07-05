52ffd304a678daf80fa23f0c896f85ed38c388d2
typoverflow <typoverflow@outlook.com>
2022-07-05 21:19:11 +0800

    update: full transformer encoder impl.

--- UtilsRL/net/attention.py
+++ UtilsRL/net/attention.py
@@ -1,3 +1,4 @@
+from turtle import back
 from typing import Any, Dict, List, Optional, Sequence, Tuple, Type, Union
 
 import torch
@@ -13,7 +14,7 @@ class Attention(nn.Module):
         n_head: int, 
         scale: bool = True, 
         self_attention: bool = True, 
-        dropout: float = 0.1, 
+        dropout: Optional[float] = None, 
     ):
         if embed_dim % n_head != 0:
             raise ValueError(f"Attention embed dim must be divisible by n_head, found {output_dim} and {n_head}.")
@@ -28,8 +29,11 @@ class Attention(nn.Module):
         else:
             self.to_context = EnsembleLinear(input_dim, self.head_dim, n_head*2)
             self.to_query = EnsembleLinear(input_dim, self.head_dim, n_head)
-        self.attn_dropout = nn.Dropout(dropout)
-        self.proj_dropout = nn.Dropout(dropout)
+        if dropout:
+            self.attn_dropout = nn.Dropout(dropout)
+            self.proj_dropout = nn.Dropout(dropout)
+        else:
+            self.attn_dropout = self.proj_dropout = nn.Identity()
         self.proj = nn.Linear(embed_dim, embed_dim)
         
     def _do_attention(
@@ -77,4 +81,86 @@ class Attention(nn.Module):
         
         return attn_value, attn_weight if return_attn_weight else attn_value
     
+
+# How about not implementing decoder for now ?
+class TransformerBlock(nn.Module):
+    def __init__(
+        self, 
+        input_dim: int, 
+        embed_dim: int, 
+        n_head: int, 
+        backbone_dim: Optional[int] = None, 
+        scale: bool = True, 
+        self_attention: bool = True, 
+        dropout: Optional[float] = None
+    ): 
+        if backbone_dim is None:
+            backbone_dim = 4 * embed_dim
+        self.attention = Attention(input_dim, embed_dim, n_head, scale, self_attention, dropout)
+        self.layer_norm1 = nn.LayerNorm(input_dim)
+        self.layer_norm2 = nn.LayerNorm(embed_dim)
+        self.ff = nn.Sequential(
+            nn.Linear(embed_dim, backbone_dim), 
+            nn.ReLU(), 
+            nn.Linear(backbone_dim, embed_dim), 
+            nn.Dropout(dropout) if dropout else nn.Identity()
+        )
+        
+    def forward(
+        self, 
+        input: torch.Tensor, 
+        query_input: Optional[torch.Tensor] = None, 
+        attention_mask: Optional[Any] = None, 
+        query_attention_mask: Optional[Any] = None, 
+        head_mask: Optional[Any] = None, 
+        return_attn_weight: bool = False
+    ): 
+        residual = input
+        attn_outputs = self.attention.forward(
+            self.layer_norm1(input), 
+            query_input=None, 
+            attention_mask=attention_mask, 
+            head_mask=head_mask, 
+            return_attn_weight=return_attn_weight
+        )
+        if return_attn_weight:
+            attn_value, attn_weight = attn_outputs
+        else:
+            attn_value = attn_outputs
+        attn_value += residual
+        
+        residual = attn_value
+        attn_value = self.ff(self.layer_norm2(attn_value))
+        attn_value = attn_value + residual
+        
+        return attn_value, attn_weight if return_attn_weight else attn_value
+
+
+class Transformer(nn.Module):
+    def __init__(
+        self, 
+        input_dim: int, 
+        embed_dim: int, 
+        n_head: int, 
+        n_layer: int, 
+        output_dim: int = 0
+    ):
+        self.embed_layer = nn.Linear(input_dim, embed_dim)
+        blocks = []
+        for i in range(n_layer):
+            blocks.append(TransformerBlock(
+                embed_dim, embed_dim, n_head, 
+            ))
+        self.transformer_blocks = nn.Sequential(*blocks)
+        if output_dim > 0:
+            self.output_layer = nn.Linear(embed_dim, output_dim)
+        else:
+            self.output_layer = nn.Identity()
+        
+    def forward(
+        self, 
+        input: torch.Tensor, 
+    ):
+        input = self.output_layer(self.transformer_blocks(self.embed_layer(input)))
+        
         
