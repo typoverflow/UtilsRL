e8ce83c9fd55a342bdae9f02c95f1b2ba4d34bb6
typoverflow <typoverflow@outlook.com>
2023-01-17 16:35:32 +0800

    feat: wand sync run function

--- UtilsRL/misc/chore.py
+++ UtilsRL/misc/chore.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import numpy as np
 
@@ -22,4 +23,29 @@ def convert_data_to_tensor(data):
     
 def convert_data_to_numpy(data):
     return np.asarray(data)
+    
+def wandb_sync_run(root_path: str, *args):
+    import wandb
+    from UtilsRL.logger import logger
+    
+    if not os.path.exists(root_path):
+        raise FileNotFoundError(f"wandb_sync_offline: path {root_path} not found.")
+    
+    def recursive_upload(path):
+        if os.path.isfile(path):
+            return
+        for sub_path in os.listdir(path):
+            full_path = os.path.join(path, sub_path)
+            if sub_path == "wandb":
+                final_path = None
+                for sub_sub_path in os.listdir(full_path):
+                    if sub_sub_path.startswith("run-") or sub_sub_path.startswith("offline-run-"):
+                        final_path = os.path.join(full_path, sub_sub_path)
+                if final_path:
+                    logger.warning(f"sync {final_path} to wandb ...")
+                    os.system(f"wandb sync {final_path} {' '.join(args)}")
+            else:
+                recursive_upload(full_path)
+    recursive_upload(root_path)
+    
     
