fcd37785e5bd532f1f037fdc3b139dbec35998fa
孔锐 <kongrui.chn@outlook.com>
2023-02-22 18:04:25 +0800

    feat: a more powerful profile which supports context-manager mode; fixed the timing issue with cuda (#27)
    * Enable profile decorator to be context manager
    
    * fix: profile proto
    
    * update: profile pass test
    
    * Enable profile to automatically record code block position
    
    * update: add pause and resume for profile, add docstring
    
    ---------
    
    Co-authored-by: typoverflow <typoverflow@outlook.com>

--- UtilsRL/misc/decorator.py
+++ UtilsRL/misc/decorator.py
@@ -1,6 +1,12 @@
-import functools
+from typing import Callable, Any, Optional
+
+import os
 import time
+import torch
 import atexit
+import traceback
+import functools
+
 
 def depreciated(func):
     """
@@ -9,7 +15,9 @@ def depreciated(func):
     def wrapper(*args, **kwargs):
         print("Depreciated function: {}".format(func.__name__))
         return func(*args, **kwargs)
+
     return wrapper
+
 
 def untested(func):
     """
@@ -18,7 +26,9 @@ def untested(func):
     def wrapper(*args, **kwargs):
         print("Untested function: {}".format(func.__name__))
         return func(*args, **kwargs)
+
     return wrapper
+
 
 def retry(retry_times, fallback):
     def decorator(func):
@@ -33,35 +43,108 @@ def retry(retry_times, fallback):
                 except KeyboardInterrupt as k:
                     raise k
                 except Exception as e:
-                    print(f"Exception occurred in {func.__name__}, has tried {try_times} times")
+                    print(
+                        f"Exception occurred in {func.__name__}, has tried {try_times} times"
+                    )
                     continue
-            print(f"Exception occurred in {func.__name__}, calling fallback {fallback.__name__}")
+            print(
+                f"Exception occurred in {func.__name__}, calling fallback {fallback.__name__}"
+            )
             if fallback is not None:
                 return fallback(*args, **kwargs)
         return wrapper
     return decorator
 
+
 def fallback(func):
     return retry(1, func)
 
+
+class profile(object):
+    """
+    Decorator to profile the function and to print elapsed time at the exit of the program, can
+    also function as a context manager to time a piece of wrapped codes within `with` statement. 
     
-def profile(func):
+    Notes
+    -----
+    Usage of this module: 
+    1. decorating a named function by `@profile(identifier=xxx)`
+    2. wrap the codes to time by `with` statement `with profile(identifier=xxx) as p:`
+    
+    Parameters
+    ----------
+    identifier :  The unique tag for disguishing the profile from others. If set to None, in decorator mode
+        the identifier will be set to the name of the decotated function, while in context-manager mode it 
+        will be set to the calling position. 
+    activate :  Whether the profile is activated or not. Default is True. 
     """
-    Decorator to profile the function and to print elapsed time at the exit of the program
-    """
-    called_times = 0
-    elapsed_time = 0
-    def exit_logger():
-        from UtilsRL.logger import logger  # the input is moved here to avoid circular import
-        logger.info(f"[Profile]: Function {func.__name__}, called {called_times} times, total elapsed time {elapsed_time}(s), avg {elapsed_time/called_times if called_times else 0}(s).")
-    atexit.register(exit_logger)
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        beg = time.time()
-        ret = func(*args, **kwargs)
-        nonlocal called_times
-        nonlocal elapsed_time
-        elapsed_time += time.time() - beg
-        called_times += 1
-        return ret
-    return wrapper
+    global_registry = {}
+
+    def __init__(self, identifier: Optional[str] = None, activate: bool = True) -> None:
+        self.identifier = identifier
+        self.activate = activate
+        self.stash = 0   # only used for context manager
+
+        def exit_logger():
+            from UtilsRL.logger import logger  # the input is moved here to avoid circular import
+            elapsed_time, called_time = profile.global_registry[self.identifier]
+            logger.info(
+                f"[profile]: Executed {self.identifier} for {called_time} times, total elapsed time {elapsed_time}(s), avg {elapsed_time/called_time if called_time else 0}(s)."
+            )
+        self.exit_logger_fn = exit_logger
+
+    def __call__(self, func: Callable) -> Callable:
+        if self.activate:
+            if self.identifier is None:
+                self.identifier = func.__name__
+            if self.identifier not in profile.global_registry:
+                profile.global_registry[self.identifier] = [0, 0]
+                atexit.register(self.exit_logger_fn)
+
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                torch.cuda.synchronize()
+                beg = time.time()
+                ret = func(*args, **kwargs)
+                torch.cuda.synchronize()
+                profile.global_registry[self.identifier][0] += time.time() - beg
+                profile.global_registry[self.identifier][1] += 1
+                return ret
+
+            return wrapper
+        else:
+            return lambda fn: fn
+
+    def __enter__(self):
+        if self.activate:
+            if self.identifier is None:
+                stack = traceback.extract_stack()[0]
+                cwd = os.getcwd()
+                filename = stack.filename
+                lineno = stack.lineno
+                if filename.startswith(cwd):
+                    filename = filename.replace(cwd, ".")
+                self.identifier = "{}: line {}".format(filename, lineno)
+            if self.identifier not in profile.global_registry:
+                profile.global_registry[self.identifier] = [0, 0]
+                atexit.register(self.exit_logger_fn)
+            torch.cuda.synchronize()
+            self.beg = time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_value: Any, traceback: Any) -> None:
+        if self.activate:
+            torch.cuda.synchronize()
+            profile.global_registry[self.identifier][0] += time.time() - self.beg + self.stash
+            profile.global_registry[self.identifier][1] += 1
+            
+    def pause(self):
+        torch.cuda.synchronize()
+        self.stash += time.time() - self.beg
+        self.beg = None
+        
+    def resume(self):
+        torch.cuda.synchronize()
+        self.beg = time.time()
+        
+        
