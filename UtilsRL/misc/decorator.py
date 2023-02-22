ab3ccab855e08eaa63ee415eb9689b86988acc86
typoverflow <typoverflow@outlook.com>
2023-02-22 13:49:06 +0800

    fix: profile proto

--- UtilsRL/misc/decorator.py
+++ UtilsRL/misc/decorator.py
@@ -2,14 +2,13 @@ import functools
 import time
 import atexit
 import torch
-from typing import Callable, Any
+from typing import Callable, Any, Optional
 
 
 def depreciated(func):
     """
     Decorator to print depreciated message.
     """
-
     def wrapper(*args, **kwargs):
         print("Depreciated function: {}".format(func.__name__))
         return func(*args, **kwargs)
@@ -21,7 +20,6 @@ def untested(func):
     """
     Decorator to hint that the function is untested.
     """
-
     def wrapper(*args, **kwargs):
         print("Untested function: {}".format(func.__name__))
         return func(*args, **kwargs)
@@ -30,9 +28,7 @@ def untested(func):
 
 
 def retry(retry_times, fallback):
-
     def decorator(func):
-
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             try_times = 0
@@ -53,9 +49,7 @@ def retry(retry_times, fallback):
             )
             if fallback is not None:
                 return fallback(*args, **kwargs)
-
         return wrapper
-
     return decorator
 
 
@@ -67,13 +61,11 @@ class profile(object):
     """
     Decorator to profile the function and to print elapsed time at the exit of the program
     """
+    global_registry = {}
 
-    def __init__(self, name: str = "default") -> None:
+    def __init__(self, name: Optional[str]=None, activate: bool=True) -> None:
         self.name = name
-        self.called_times = 0
-        self.total_time = 0
-        self.actived = False
-        self.start = 0
+        self.activate = activate
 
         def exit_logger():
             from UtilsRL.logger import logger  # the input is moved here to avoid circular import
@@ -82,30 +74,39 @@ class profile(object):
                 f"[profile]: Executed {self.name} {self.called_times} times, total elapsed time {self.total_time}(s), avg {self.total_time/self.called_times if self.called_times else 0}(s)."
             )
 
-        atexit.register(exit_logger)
+        if self.activate:
+            atexit.register(exit_logger)
 
-    def __call__(self, func: Callable, name: str = "default") -> None:
-        if name != "default":
-            self.name = name
+    def __call__(self, func: Callable) -> Callable:
+        if self.activate:
+            if self.name is None:
+                self.name = func.__name__
+            profile.global_registry[self.name] = [0, 0]
+
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                torch.cuda.synchronize()
+                beg = time.time()
+                ret = func(*args, **kwargs)
+                torch.cuda.synchronize()
+                profile.global_registry[self.name][0] += time.time() - beg
+                profile.global_registry[self.name][1] += 1
+                return ret
+
+            return wrapper
         else:
-            self.name = func.__name__
-
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            torch.cuda.synchronize()
-            beg = time.time()
-            ret = func(*args, **kwargs)
-            self.total_time += time.time() - beg
-            self.called_times += 1
-            return ret
-
-        return wrapper
+            return lambda fn: fn
 
     def __enter__(self) -> None:
-        torch.cuda.synchronize()
-        self.start = time.time()
-        self.called_times += 1
+        if self.activate:
+            if self.name is None:
+                self.name = "default"
+            torch.cuda.synchronize()
+            self.beg = time.time()
 
     def __exit__(self, exc_type, exc_value: Any, traceback: Any) -> None:
-        torch.cuda.synchronize()
-        self.total_time += time.time() - self.start
+        if self.activate:
+            torch.cuda.synchronize()
+            profile.global_registry[self.name][0] += time.time() - self.beg
+            profile.global_registry[self.name][1] += 1
+        
