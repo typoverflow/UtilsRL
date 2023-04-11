53e99b96aa1aa1ebc35a5976ae72a52ac26692ab
typoverflow <typoverflow@outlook.com>
2023-04-11 11:02:54 +0800

    feat: log histogram for tb logger

--- UtilsRL/__init__.py
+++ UtilsRL/__init__.py
@@ -1,3 +1,15 @@
 
 __version__ = "0.5.5"
 
+from UtilsRL import data_structure, env, exp, logger, math, misc, plot, rl, monitor
+__all__ = [
+    "data_structure", 
+    "env", 
+    "exp", 
+    "logger", 
+    "math", 
+    "misc", 
+    "plot", 
+    "rl", 
+    "monitor"
+]
