28449da6973122acb64f924f545ce234dc68322b
typoverflow <typoverflow@outlook.com>
2023-04-19 19:15:50 +0800

    update: RWKV main code

--- UtilsRL/net/attention/rwkv.py
+++ UtilsRL/net/attention/rwkv.py
@@ -1,4 +1,3 @@
-import math, os
 import numpy as np
 import torch
 import torch.nn as nn
@@ -9,8 +8,8 @@ from typing import Any, Dict, List, Optional, Sequence, Tuple, Type, Union
 T_MAX = 1024
 
 wkv_op = load(
-    name="wkv",
-    sources=["UtilsRL/operator/wkv_op.cpp", "UtilsRL/operator/wkv_op.cu"],
+    name="wkv_extend",
+    sources=["UtilsRL/operator/wkv_op_extend.cpp", "UtilsRL/operator/wkv_op_extend.cu"],
     verbose=True,
     extra_cuda_cflags=[
         '-res-usage',
@@ -31,6 +30,7 @@ class WKV(torch.autograd.Function):
         ctx.C = C
         assert T <= T_MAX
         assert B * C % min(C, 1024) == 0
+        device = w.device
         w = - torch.exp(w.contiguous())
         u = u.contiguous()
         k = k.contiguous()
@@ -38,13 +38,16 @@ class WKV(torch.autograd.Function):
         h1 = h1.contiguous()
         h2 = h2.contiguous()
         h3 = h3.contiguous()
-        y = torch.empty((B, T, C), device=w.device, memory_format=torch.contiguous_format)
-        wkv_op.forward(B, T, C, w, u, k, v, h1, h2, h3, y)
+        y = torch.empty((B, T, C), device=device, memory_format=torch.contiguous_format)
+        oh1 = torch.empty((B, C), device=device, memory_format=torch.contiguous_format)
+        oh2 = torch.empty((B, C), device=device, memory_format=torch.contiguous_format)
+        oh3 = torch.empty((B, C), device=device, memory_format=torch.contiguous_format)
+        wkv_op.forward(B, T, C, w, u, k, v, h1, h2, h3, y, oh1, oh2, oh3)
         ctx.save_for_backward(w, u, k, v, h1, h2, h3, y)
-        return y
+        return y, oh1, oh2, oh3
 
     @staticmethod
-    def backward(ctx: Any, gy: Any) -> Any:
+    def backward(ctx: Any, gy, oh1, oh2, oh3) -> Any:
         B = ctx.B
         T = ctx.T
         C = ctx.C
@@ -59,11 +62,11 @@ class WKV(torch.autograd.Function):
         wkv_op.backward(B, T, C, w, u, k, v, h1, h2, h3, y, gy.contiguous(), gw, gu, gk, gv)
         gw = torch.sum(gw, dim=0)
         gu = torch.sum(gu, dim=0)
-        return (None, None, None, gw, gu, gk, gv)
+        return (None, None, None, gw, gu, gk, gv, None, None, None)
         
 
-def RUN_CUDA(B, T, C, w, u, k, v):
-    return WKV.apply(B, T, C, w.cuda(), u.cuda(), k.cuda(), v.cuda())
+def RUN_CUDA(B, T, C, w, u, k, v, h1, h2, h3):
+    return WKV.apply(B, T, C, w, u, k, v, h1, h2, h3)
 
 
 class RWKVChannelMix(nn.Module):
@@ -97,11 +100,26 @@ class RWKVTimeMix(nn.Module):
         self.time_decay = nn.Parameter(torch.ones([embed_dim, ]))
         self.time_first = nn.Parameter(torch.ones([embed_dim, ]))
         
-    def forward(self, input: torch.Tensor):
+    def forward(
+        self, 
+        input: torch.Tensor, 
+        hidden: Optional[torch.Tensor]=None, 
+        cell_state: Optional[torch.Tensor]=None, 
+    ):
         B, T, C = input.shape
         r, v, k = torch.split(self.rvk(input), 3)
-        rwkv = torch.sigmoid(r) * RUN_CUDA(B, T, C, self.time_decay, self.time_first, k, v)
-        return self.output(rwkv)
+        if hidden is None:
+            hidden = torch.zeros([B, C]).to(input.device)
+        if cell_state is None:
+            h2 = torch.zeros([B, C]).to(input.device)
+            h3 = torch.full([B, C], fill_value=-1e38).to(input.device)
+        else:
+            h2, h3 = torch.split(cell_state, 2, -1)
+        
+        wkv, h1, h2, h3 = RUN_CUDA(B, T, C, self.time_decay, self.time_first, k, v, hidden, h2, h3)
+        rwkv = torch.sigmoid(r) * wkv
+        c = torch.concat([h2, h3], dim=-1)
+        return self.output(rwkv), h1, c
     
 
 class RWKVBlock(nn.Module):
@@ -115,11 +133,17 @@ class RWKVBlock(nn.Module):
         self.attention = RWKVTimeMix(embed_dim)
         self.ffn = RWKVChannelMix(embed_dim)
         
-    def forward(self, input: torch.Tensor):
+    def forward(
+        self, 
+        input: torch.Tensor, 
+        hidden: Optional[torch.Tensor]=None, 
+        cell_state: Optional[torch.Tensor]=None
+    ):
         residual = input
-        residual = residual + self.attention(self.ln1(residual))
+        input, h, c = self.attention(self.ln1(input), hidden=hidden, cell_state=cell_state)
+        residual = residual + input
         residual = residual + self.ffn(self.ln2(residual))
-        return residual
+        return residual, h, c
     
     
 class RWKV(nn.Module):
@@ -146,16 +170,29 @@ class RWKV(nn.Module):
     def forward(
         self, 
         inputs: torch.Tensor, 
+        rwkv_hiddens: Optional[None], 
+        rwkv_cell_states: Optional[None], 
         do_embedding: bool=True
     ):
         if do_embedding:
             inputs = self.input_embed(inputs)
         inputs = self.ln_in(inputs)
-        inputs = self.blocks(inputs)
+        if rwkv_hiddens is not None and rwkv_cell_states is not None:
+            rwkv_hiddens = torch.split(rwkv_hiddens, len(self.blocks))
+            rwkv_cell_states = torch.split(rwkv_hiddens, len(self.blocks))
+        else:
+            rwkv_hiddens = [None] * len(self.blocks)
+            rwkv_cell_states = [None] * len(self.blocks)
+        new_hiddens = []
+        new_cell_states = []
+        for idx, block in enumerate(self.blocks):
+            inputs, h, c = block(inputs, rwkv_hiddens[idx], rwkv_cell_states[idx])
+            new_hiddens.append(h)
+            new_cell_states.append(c)
+        
         inputs = self.output(self.ln_out(inputs))
-        return inputs
-        
-        
-        
+        new_hiddens = torch.concat(new_hiddens, dim=0)
+        new_cell_states = torch.concat(new_cell_states, dim=0)
+        return inputs, new_hiddens, new_cell_states
         
         
