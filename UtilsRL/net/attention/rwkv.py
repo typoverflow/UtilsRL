760d298aaa030a64db8a5b96b29d17e9f6f3b841
typoverflow <typoverflow@outlook.com>
2023-04-19 19:48:52 +0800

    update: DecisionRWKV

--- UtilsRL/net/attention/rwkv.py
+++ UtilsRL/net/attention/rwkv.py
@@ -196,3 +196,52 @@ class RWKV(nn.Module):
         return inputs, new_hiddens, new_cell_states
         
         
+class DecisionRWKV(RWKV):
+    def __init__(
+        self,
+        obs_dim: int, 
+        action_dim: int, 
+        embed_dim: int, 
+        num_layers: int, 
+        seq_len: int, 
+        episode_len: int, 
+    ) -> None:
+        super().__init__(
+            input_dim=embed_dim, # actually not used
+            embed_dim=embed_dim, 
+            num_layers=num_layers, 
+            output_dim=0 # we manually handle the output outside
+        )
+        # RWKV only encode relative timestep but not absolute timestep, so here we still need to  embed
+        # the absolute timestep info
+        self.pos_embed = nn.Linear(1, embed_dim)
+        self.obs_embed = nn.Linear(obs_dim, embed_dim)
+        self.act_embed = nn.Linear(action_dim, embed_dim)
+        self.ret_embed = nn.Linear(1, embed_dim)
+        self.action_head = nn.Sequential(nn.Linear(embed_dim, action_dim), nn.Tanh())
+
+    def forward(
+        self, 
+        states: torch.Tensor, 
+        actions: torch.Tensor, 
+        returns_to_go: torch.Tensor, 
+        timesteps: torch.Tensor, 
+        hiddens: Optional[torch.Tensor]=None, 
+        cell_states: Optional[torch.Tensor]=None
+    ):
+        B, L, *_ = states.shape
+        time_embedding = self.pos_embed(timesteps)
+        state_embedding = self.obs_embed(states) + time_embedding
+        action_embedding = self.act_embed(actions) + time_embedding
+        return_embedding = self.ret_embed(returns_to_go) + time_embedding
+        stacked_input = torch.stack([return_embedding, state_embedding, action_embedding], dim=2).reshape(B, 3*L, state_embedding.shape(-1))
+        out = super().forward(
+            stacked_input, 
+            hiddens, 
+            cell_states, 
+            do_embedding=False
+        )
+        out = self.action_head(out[:, 1::3])
+        return out
+        
+        
